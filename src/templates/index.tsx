239467eaf40069eb8d19c0afba36ae6335baf1d1
saicharanarishanapally <34207825+saicharanarishanapally@users.noreply.github.com>
2020-09-23 01:57:54 +0530

    update

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -140,7 +140,7 @@ const IndexPage: React.FC<IndexProps> = props => {
               <SiteTitle>
                 {props.data.logo ? (
                   <img
-                    style={{ maxHeight: '45px' }}
+                    style={{ maxHeight: '15px' }}
                     src={props.data.logo.childImageSharp.fixed.src}
                     alt={config.title}
                   />
